3f986286f7457f9abbc3c7ac4d4fedc3fa222df2
malindu-w <malinduweerasooriya@gmail.com>
2021-08-08 21:28:43 +0530

    Update

--- components/vaccinationconst/vaccinationconst-controller.js
+++ components/vaccinationconst/vaccinationconst-controller.js
@@ -4,16 +4,23 @@ var trackerCapture = angular.module('trackerCapture');
 trackerCapture.controller('VaccinationConstController',
     function (
         $scope,
-        SessionStorageService
+        CurrentSelection,
+        SessionStorageService,
+        OptionSetService
     ) {
+        
+        var selections = CurrentSelection.get().optionSets;
 
         $scope.vaccineOptions = {
             product: null,
             batch: null,
         };
 
-        var vaccineProductOptions = [];
-        var vaccineBatchOptions = [];
+        $scope.vaccineProductOptions = selections['m8kSC9Sl2mE'].options;
+        $scope.vaccineBatchOptions = selections['Sx0iIwAsj5S'].options;
+
+        console.log('test');
+        console.log(selections);
 
         $scope.saveOptions = () => {
             SessionStorageService.set('VACCINE_PRODUCT', $scope.vaccineOptions.product);
