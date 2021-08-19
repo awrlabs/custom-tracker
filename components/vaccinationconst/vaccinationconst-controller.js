15323bc58ff01d308ac5d7ad7cef872cf98329dd
malindu-w <malinduweerasooriya@gmail.com>
2021-08-19 23:57:48 +0530

    Console log removal

--- components/vaccinationconst/vaccinationconst-controller.js
+++ components/vaccinationconst/vaccinationconst-controller.js
@@ -19,9 +19,6 @@ trackerCapture.controller('VaccinationConstController',
         $scope.vaccineProductOptions = selections['m8kSC9Sl2mE'].options;
         $scope.vaccineBatchOptions = selections['Sx0iIwAsj5S'].options;
 
-        console.log('test');
-        console.log(selections);
-
         $scope.saveOptions = () => {
             SessionStorageService.set('VACCINE_PRODUCT', $scope.vaccineOptions.product);
             SessionStorageService.set('VACCINE_BATCH', $scope.vaccineOptions.batch);
