1fe540368e98cf197b3b8e94e0816140f8f2c313
awrlabs <awrlabs@awrlabs.lk>
2021-08-29 09:06:11 +0530

    temp changes

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ try {
     console.warn('\nWARNING! Failed to load DHIS config:', e.message);
     console.info('Using default config');
     dhisConfig = {
-        baseUrl: 'http://135.181.131.10/test1/',
+        baseUrl: 'https://covid-19.health.gov.lk/vaccine/',
         certUrl: 'http://localhost:8080/',
         authorization: process.env.DHIS_AUTH // admin:district
     };
